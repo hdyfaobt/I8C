2f7e7d33d9d2f6d7824f156d6dce63aeceb205ad
hdyfaobt <145970301+hdyfaobt@users.noreply.github.com>
2026-07-08 02:05:33 +0200

    FEAT: add details link to customer list

--- resources/views/userzone/customers/index.blade.php
+++ resources/views/userzone/customers/index.blade.php
@@ -64,6 +64,10 @@
                                     @endif
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-2">
+                                    {{-- Detail button --}}
+                                    <a href="{{ route('customers.show', $customer) }}"
+                                       class="text-indigo-600 hover:text-indigo-900">Details</a>
+
                                     {{-- Edit button --}}
                                     <a href="{{ route('customers.edit', $customer) }}"
                                        class="text-indigo-600 hover:text-indigo-900">Bewerken</a>
