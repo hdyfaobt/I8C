5988bf93c376414abc56545d81559711d610872c
hdyfaobt <145970301+hdyfaobt@users.noreply.github.com>
2026-07-08 02:08:33 +0200

    FEAT: apply Pint code style to CustomerFactory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,19 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ * @extends Factory<Customer>
  */
 class CustomerFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'name'    => fake()->name(),
-            'email'   => fake()->unique()->safeEmail(),
-            'phone'   => fake()->phoneNumber(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
             'company' => fake()->company(),
             'address' => fake()->address(),
         ];
