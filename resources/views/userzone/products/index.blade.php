b141b6cabc89e5e02ce9c5b5be3ac08e37531936
hdyfaobt <145970301+hdyfaobt@users.noreply.github.com>
2026-07-26 02:48:03 +0100

    FORMAT: widen products list layout

--- resources/views/userzone/products/index.blade.php
+++ resources/views/userzone/products/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+        <div class="px-6 lg:px-8">
 
             {{-- Success message --}}
             @if (session('success'))
@@ -27,8 +27,7 @@
             </div>
 
             {{-- Products table --}}
-            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
-                <table class="min-w-full divide-y divide-gray-200">
+            <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                         <tr>
                             <x-sortable-header column="article_number" label="Artikelnummer" />
@@ -77,7 +76,6 @@
                         @endforelse
                     </tbody>
                 </table>
-            </div>
 
         </div>
     </div>
