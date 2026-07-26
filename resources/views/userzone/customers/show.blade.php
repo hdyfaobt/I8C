cbb322aecfc22c2264366f72e664b32345f13224
hdyfaobt <145970301+hdyfaobt@users.noreply.github.com>
2026-07-26 02:52:54 +0100

    FEAT: add deleting state to customer delete modal

--- resources/views/userzone/customers/show.blade.php
+++ resources/views/userzone/customers/show.blade.php
@@ -8,7 +8,7 @@
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6"
-             x-data="{ deleteModalOpen: false, deleteForm: null }">
+             x-data="{ deleteModalOpen: false, deleteForm: null, deleting: false }">
 
             {{-- Success message --}}
             @if (session('success'))
