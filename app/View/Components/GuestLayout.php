b460d4d2eca6178a4c2a8fd62d519ba69eabd1ea
hdyfaobt <145970301+hdyfaobt@users.noreply.github.com>
2026-08-05 23:54:02 +0200

    FORMAT: trim docblock in GuestLayout component

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -7,9 +7,7 @@ use Illuminate\View\View;
 
 class GuestLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     */
+    // Render guest layout view
     public function render(): View
     {
         return view('layouts.guest');
